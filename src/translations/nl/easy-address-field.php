bd0e3e9873e9365ac6c3a9039412cf926c275e88
Jan Henckens <jan@henckens.be>
2024-02-14 20:38:23 +0100

    Translation updat

---
+++ src/translations/nl/easy-address-field.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'Enable' => 'Tonen',
+    'Field' => 'Veld',
+    'Name' => 'Naam',
+    'Street 2' => 'Straat, 2de regel',
+    'State' => 'Staat / Provincie',
+    'Country' => 'Land',
+    'Default country' => 'Standaard geselecteerde land',
+    'Get coordinates for addresses' => 'Coördinaten ophalen voor het ingevulde adres',
+    'Show coordinates?' => 'Coördinaten tonen',
+    'Street' => 'Straat',
+    'Postal code' => 'Postcode',
+    'City' => 'Stad of gemeente',
+    'Pick an address on the map' => 'Kies een adres op de kaart',
+
+];
